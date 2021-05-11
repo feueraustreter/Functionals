7093318e7e4e281cb5a0871233e16613aec644b1
yoyosource <yoyosource@nidido.de>
2021-05-11 10:59:07 +0200

    Add JavaDoc

--- src/main/java/feueraustreter/stream/Sink.java
+++ src/main/java/feueraustreter/stream/Sink.java
@@ -2,6 +2,19 @@ package feueraustreter.stream;
 
 import java.util.function.Consumer;
 
+/**
+ * This class is used internally and could be represented by a {@link Consumer}
+ * but was not as this more clearly depicts what this class should do. It accepts
+ * any element of the previous operation and can do something to it if necessary.
+ *
+ * @param <T> the type it accepts.
+ */
 @FunctionalInterface
-public interface Sink<T> extends Consumer<T> {
+public interface Sink<T> {
+    /**
+     * Performs this operation on the given argument.
+     *
+     * @param t the input argument
+     */
+    void accept(T t);
 }
