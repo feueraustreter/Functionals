29b6a2982c61de4a1cfd457fd1e54198fc325ccb
yoyosource <yoyosource@nidido.de>
2021-05-12 11:30:47 +0200

    Add FunctionalStreamDistinctTest Add FunctionalStreamFilterTest Add FunctionalStreamMapTest

---
+++ src/test/java/feueraustreter/stream/FunctionalStreamDistinctTest.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2021 feueraustreter
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package feueraustreter.stream;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class FunctionalStreamDistinctTest {
+
+    @Test
+    public void testRemoveAll() {
+        List<String> stringList = new ArrayList<>();
+        stringList.add("1");
+        stringList.add("2");
+        stringList.add("3");
+        stringList.add("3");
+
+        List<String> result = FunctionalStream.of(stringList)
+                .distinct()
+                .toList();
+        assertThat(result.size(), is(3));
+    }
+
+}
